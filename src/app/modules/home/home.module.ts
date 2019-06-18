7c2f8df0c2adee2aca94e754bbde86df692f895b
Mingda Jin <jinmingda77@gmail.com>
2019-06-18 11:06:20 -0700

    Add non-export subcomponent for HomePage
    `ng g c modules/home/components/section-top` automatically declares
    component in HomeModule

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SectionTopComponent } from './components/section-top/section-top.component';
 
 @NgModule({
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, SectionTopComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
