1c6e5e9cb80e4d295f4be5797f653f37b2ccab71
Mingda Jin <jinmingda77@gmail.com>
2019-06-18 02:26:28 -0700

    Get currentYear dynamically for footer
    Use DatePipe in template

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  currentYear: Date;
+
   constructor() { }
 
   ngOnInit() {
+    this.currentYear = new Date();
   }
 
 }
