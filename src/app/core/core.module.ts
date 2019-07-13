7d2e8960aa6f79dc6d4ff9b059c0c4b6c8bec62b
Mingda Jin <jinmingda77@gmail.com>
2019-07-12 21:59:27 -0700

    Create NotFound component by ng command
    `ng g  component core/not_found --export`

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, NotFoundComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [HeaderComponent, FooterComponent, NotFoundComponent]
 })
 export class CoreModule { }
